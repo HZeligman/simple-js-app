ba77c4f38aeb13538b1145782d034e5282e0e813
HZeligman <103866815+HZeligman@users.noreply.github.com>
2022-05-08 12:16:52 -0400

    Added if, then loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,28 @@
-const pokemonList = [
+let pokemonList= [
   {
     name: 'Eevee',
-  height: 0.3,
-  weight: 6.5,
-  type: ['normal'],
-  }
-
+    height: 0.3,
+    weight: 6.5,
+    type: ['normal'],
+  },
   {
     name: 'Quilava',
-  height: 0.9,
-  weight: 19,
-  type: ['fire'],
-  }
-
+    height: 0.9,
+    weight: 19,
+    type: ['fire'],
+  },
   {
     name: 'Charizard',
-  height: 1.7,
-  weight: 90.5,
-  type: ['fire' 'flying'],
-  }
+    height: 1.7,
+    weight: 90.5,
+    type: ['fire', 'flying'],
+  },
 ]
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height >1){
+    document.write(pokemonList[i].name + "(height: pokemonList[i].height)" + "Wow he's tall!");
+  } else {
+    document.write(pokemonList[i].name + "(height: pokemonList[i].height)");
+  }
+}
