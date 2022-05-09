20d6e7aa41c9d9ffbed6af229d1a63e1a2176f79
HZeligman <103866815+HZeligman@users.noreply.github.com>
2022-05-09 11:01:18 -0400

    Fixed IIFE and forEach function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,25 @@
-let pokemonList= [
-  { name: 'Eevee', height: 0.3, weight: 6.5, type: ['normal'] },
-  { name: 'Quilava', height: 0.9, weight: 19, type: ['fire'], },
-  { name: 'Charizard', height: 1.7, weight: 90.5, type: ['fire', 'flying'] },
-]
-
 let pokemonRepository = (function () {
-  let pokemonList = []; // empty array
+  let pokemonList= [
+    { name: 'Eevee', height: 0.3, weight: 6.5, type: ['normal'] },
+    { name: 'Quilava', height: 0.9, weight: 19, type: ['fire'], },
+    { name: 'Charizard', height: 1.7, weight: 90.5, type: ['fire', 'flying'] },
+  ]
 
   return {
     add: function(pokemon) {
       pokemonList.push(pokemon);
     },
-    getAll: function() {
+    getAll: function(pokemon) {
       return pokemonList;
     }
   };
 })();
 
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.add({name: 'Marill', height: 0.4, weight: 8.5, type: ['fairy', 'water']});
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function(pokemon) {
   if (pokemon.height >1){
     document.write("<p>" + pokemon.name + "(height: " + pokemon.height + ") Wow he's tall!" + "</p>");
   } else {
