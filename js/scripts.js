2b06a39b86c1609086211b3fa730c3eb5aee2442
HZeligman <103866815+HZeligman@users.noreply.github.com>
2022-05-08 21:00:09 -0400

    Simplified array and added function print array. Plus cleaned up code to display correctly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,16 @@
 let pokemonList= [
-  {
-    name: 'Eevee',
-    height: 0.3,
-    weight: 6.5,
-    type: ['normal'],
-  },
-  {
-    name: 'Quilava',
-    height: 0.9,
-    weight: 19,
-    type: ['fire'],
-  },
-  {
-    name: 'Charizard',
-    height: 1.7,
-    weight: 90.5,
-    type: ['fire', 'flying'],
-  },
+  { name: 'Eevee', height: 0.3, weight: 6.5, type: ['normal'] },
+  { name: 'Quilava', height: 0.9, weight: 19, type: ['fire'], },
+  { name: 'Charizard', height: 1.7, weight: 90.5, type: ['fire', 'flying'] },
 ]
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height >1){
-    document.write(pokemonList[i].name + "(height: pokemonList[i].height)" + "Wow he's tall!");
-  } else {
-    document.write(pokemonList[i].name + "(height: pokemonList[i].height)");
+function printArrayDetails(list){
+  for (let i = 0; i < list.length; i++) {
+    if (pokemonList[i].height >1){
+      document.write("<p>" + pokemonList[i].name + "(height: " + pokemonList[i].height + ") Wow he's tall!" + "</p>");
+    } else {
+      document.write("<p>" + pokemonList[i].name + "(height: " + pokemonList[i].height + ")</p>");
+    }
   }
 }
+printArrayDetails(pokemonList);
