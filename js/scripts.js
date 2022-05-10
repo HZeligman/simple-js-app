90d5740332807d12ec9a6676c9d5459c531f7066
HZeligman <103866815+HZeligman@users.noreply.github.com>
2022-05-09 21:32:44 -0400

    Cleaned up code and added foreach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,47 @@
 let pokemonRepository = (function () {
-  let pokemonList= [
+  let repository= [
     { name: 'Eevee', height: 0.3, weight: 6.5, type: ['normal'] },
     { name: 'Quilava', height: 0.9, weight: 19, type: ['fire'], },
     { name: 'Charizard', height: 1.7, weight: 90.5, type: ['fire', 'flying'] },
   ]
 
-  return {
-    add: function(pokemon) {
-      pokemonList.push(pokemon);
-    },
-    getAll: function(pokemon) {
-      return pokemonList;
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "weight" in pokemon &&
+      "type" in pokemon
+    ) {
+      repository.push(pokemon);
+    } else {
+      console.log("Incorrect Format");
     }
-  };
+  }
+  function getAll(pokemon) {
+    return repository;
+  }
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('button');
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem)
+  }
+
+  return {
+     add: add,
+     getAll: getAll,
+     addListItem: addListItem
+   };
 })();
 
 pokemonRepository.add({name: 'Marill', height: 0.4, weight: 8.5, type: ['fairy', 'water']});
 
 console.log(pokemonRepository.getAll());
 
-pokemonRepository.getAll().forEach(function(pokemon) {
-  if (pokemon.height >1){
-    document.write("<p>" + pokemon.name + "(height: " + pokemon.height + ") Wow he's tall!" + "</p>");
-  } else {
-    document.write("<p>" + pokemon.name + "(height: " + pokemon.height + ")</p>");
-  }
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
 });
