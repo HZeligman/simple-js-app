b532dbcb3466d3b633744ca742f63a17792306ad
HZeligman <103866815+HZeligman@users.noreply.github.com>
2022-05-10 12:47:00 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,22 @@
 let pokemonRepository = (function () {
-  let repository= [
-    { name: 'Eevee', height: 0.3, weight: 6.5, type: ['normal'] },
-    { name: 'Quilava', height: 0.9, weight: 19, type: ['fire'], },
-    { name: 'Charizard', height: 1.7, weight: 90.5, type: ['fire', 'flying'] },
-  ]
+  let pokemonList= [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
     if (
       typeof pokemon === "object" &&
-      "name" in pokemon &&
-      "height" in pokemon &&
-      "weight" in pokemon &&
-      "type" in pokemon
+      "name" in pokemon //&&
+      //"height" in pokemon &&
+      //"weight" in pokemon &&
+      //"type" in pokemon
     ) {
-      repository.push(pokemon);
+      pokemonList.push(pokemon);
     } else {
       console.log("Incorrect Format");
     }
   }
   function getAll(pokemon) {
-    return repository;
+    return pokemonList;
   }
   function addListItem(pokemon) {
     let pokemonList = document.querySelector('.pokemon-list');
@@ -29,31 +26,61 @@ let pokemonRepository = (function () {
     button.classList.add('button');
     listItem.appendChild(button);
     pokemonList.appendChild(listItem);
-    addEvent(button, pokemon);
+    button.addEventListener("click", function(event) {
+      showDetails(pokemon);})
   }
-  function addEvent(button, pokemon) {
-    button.addEventListener ('click', function (pokemon) {
-      onclick = console.log (button.innerText);
-    });
-  }
-  function showDetails(pokemon) {
-    return repository;
-  }
+
+
+  function loadList() {
+      return fetch(apiUrl).then(function (response) {
+        return response.json();
+      }).then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon);
+        });
+      }).catch(function (e) {
+        console.error(e);
+      })
+    }
+
+    function loadDetails(item) {
+      let url = item.detailsUrl;
+      return fetch(url).then(function (response) {
+        return response.json();
+      }).then(function (details) {
+        // Now we add the details to the item
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+      }).catch(function (e) {
+        console.error(e);
+      });
+    }
+
+    function showDetails(pokemon) {
+      loadDetails(pokemon).then(function () {
+        console.log(pokemon);
+      });
+    }
 
   return {
      add: add,
      getAll: getAll,
      addListItem: addListItem,
+     loadList: loadList,
+     loadDetails: loadDetails,
      showDetails: showDetails
    };
 })();
 
-pokemonRepository.add({name: 'Marill', height: 0.4, weight: 8.5, type: ['fairy', 'water']});
-
 console.log(pokemonRepository.getAll());
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
-});
-
-console.log(pokemonRepository.showDetails());
+pokemonRepository.loadList (). then(function() {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
+})
